db0f0ff591a1d921a41e8e8b983db45a6bfdad10
adhtya_b <laxusfarhami@gmail.com>
2021-02-22 16:04:55 +0700

    buat data baru dari form

--- restDemo/index.js
+++ restDemo/index.js
@@ -1,15 +1,55 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
 
-app.get('/makanan', (req,res) =>{
-    res.send("dapet makanan dari get");
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const listMakanan = [
+    {
+        namaMakanan: "mie goreng",
+        deskripsi: "mie yang di goreng"
+    },
+    {
+        namaMakanan: "mie rebus",
+        deskripsi: "mie yang di rebus"
+    },
+    {
+        namaMakanan: "nasi goreng",
+        deskripsi: "nasi yang di goreng"
+    },
+    {
+        namaMakanan: "ayam goreng",
+        deskripsi: "ayam yang di goreng"
+    }
+]
+
+app.get('/semuamakanan', (req, res)=>{
+    res.render('semuamakanan/index', {listMakanan})
 })
 
-app.post('/makanan', (req, res)=>{
-    console.log(req.body)
-    res.send("dapet makanan dari post")
+app.get('/semuamakanan/new', (req, res)=>{
+    res.render('semuamakanan/new');
+})
+
+app.post('/semuamakanan', (req, res)=>{
+    const {namaMakanan, deskripsi} = req.body;
+    listMakanan.push({namaMakanan, deskripsi});
+    res.redirect('/semuamakanan');
+})
+
+app.get('/makanan', (req, res) =>{
+    res.send('Get makanan');
+})
+
+app.post('/makanan', (req, res) =>{
+    const { makanan, jmlmakanan } = req.body;
+    res.send(`Ada ${jmlmakanan} ${makanan}`);
 })
 
 app.listen(3000,()=>{
-    console.log("hey it's on 3000");
+    console.log("on 3000")
 })
