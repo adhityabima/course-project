41fc5bec16ffb7803890039c331f56927dfd3368
adhtya_b <laxusfarhami@gmail.com>
2021-02-22 13:53:31 +0700

    first commit

---
+++ restDemo/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/makanan', (req,res) =>{
+    res.send("dapet makanan dari get");
+})
+
+app.post('/makanan', (req, res)=>{
+    res.send("dapet makanan dari post")
+})
+
+app.listen(3000,()=>{
+    console.log("hey it's on 3000");
+})
